512f478dc2360cfaee739e4b89769fccea816228
Ludoviccccc <ludovic.matar@inria.fr>
2026-05-06 14:18:31 +0200

    improve code clarity and add resume option

--- option1/history.py
+++ option1/history.py
@@ -5,48 +5,47 @@ import copy
 import random
 class History:
     def __init__(self,
-                    length_ = 10,
                     capacity:int=10000,
                     ):
-        self.memory_program = {"core0":[]}
+        self.memory_parameter = []
         self.capacity = capacity
-        self.event ={} 
+        self.memory_observation ={} 
         self._j = 0
         self.names = []
-        self.length_ = length_
         self.numpy_view = 0
         self.rand_id = random.uniform(0,1)
 
     def __eq__(self,other):
         return self.__dict__== other.__dict__
     def __getitem__(self,id_):
-        return self.memory_program['core0'][id_]
+        return self.memory_parameter[id_]
     def as_tab(self):
         if self._j==0:
             raise TypeError("no element stored yet")
         return self.numpy_view[:self._j]
     def __len__(self):
-        return len(self.memory_program["core0"])
+        return len(self.memory_parameter)
     def store(self,sample:dict,obs:dict):
         if self._j>=self.capacity:
             raise Exception("Exceeded capacity")
-        self.memory_program["core0"].append(sample)
+        self.memory_parameter.append(sample)
         tab = []
         for key in obs:
             object_ = list(obs[key].values())
             tab += object_
-            if key in self.event:
-                self.event[key].append(object_)
+            if key in self.memory_observation:
+                self.memory_observation[key].append(object_)
             else:
-                self.event[key] = [object_]
+                self.memory_observation[key] = [object_]
         if self._j ==0:
-            self.length_ = len(tab)
-            self.numpy_view = np.zeros((self.capacity,self.length_))
+            self.numpy_view = np.zeros((self.capacity,len(tab)))
         self.numpy_view[self._j] = tab
         self._j+=1
     def content(self):
         return {
-                "memory_program":self.memory_program,
+                "memory_parameter":self.memory_parameter,
+                "memory_observation":self.memory_observation,
+                "numpy_view":self.numpy_view,
                 }
     def save_pickle(self, name:str=None):
         k = 0
@@ -55,3 +54,10 @@ class History:
         output = self.content()
         with open(f"{name}_{k}.pkl", "wb") as f:
             pickle.dump(output, f)
+
+    def take(self,content,count):
+        self.memory_parameter = content["memory_parameter"][:count]
+        self.numpy_view = content["numpy_view"][:]
+        for key in content["memory_observation"]:
+            self.memory_observation[key] = content["memory_observation"][key][:count]
+        self._j = count
