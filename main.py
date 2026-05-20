723d38adabccbe2833100635022310aec0a3f525
Ludoviccccc <ludovic.matar@inria.fr>
2026-05-20 16:42:13 +0200

    creation of run_imgep function

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ from option1.distance import DistanceMethod
 from option1.mutation import MutationInstructions
 from option1.mix_interleaving import Mix_sequences_interleaved
 from option1.goal_generation import GoalGenerator
-from option1.imgep import IMGEP
+from option1.imgep import run_imgep
 from option1.env import Environment
-
 from exploration.env.func import Experiment
-
 import json
 import sys
 def print_dict(dict_):
@@ -23,27 +21,12 @@ def print_dict(dict_):
 
 
 
-class randomexploration:
-    def __init__(self,N,environment, generator,history:History,print_freq:int=1000):
-        self.generator = generator
-        self.environment = environment
-        self.N = N
-        self.history = history
-        self.print_freq = print_freq
-    def __call__(self):
-        for j in range(self.N):
-            if (j+1)%self.print_freq==0:
-                print(f'step {j+1}/{self.N}')
-            parameter = self.generator()
-            obs = self.environment(parameter)
-            self.history.store(parameter,obs)
-
-
 def distance_function(goal,features):
     x = goal
     v = x-features
     out = np.sum(v**2)
     return out
+
 
 
 if __name__=='__main__':
@@ -58,14 +41,10 @@ if __name__=='__main__':
 
     #Simulation parameters
     max_cycle = 60 #Maximum cycle in simulation
-   
-
-    with open(sys.argv[1],"rb") as f:
-         config = json.load(f)
  
     #IMGEP parameters
     capacity = 10000 #History capacity
-    k = 2 #Number of neighbors in goal achievement strategy
+    k = 1 #Number of neighbors in goal achievement strategy
     N = 1000 #Number of imgep iterations
     N_init = 100 #Number of warming iterations
     print_freq = 100 #print iteration step every print_freq
@@ -78,9 +57,10 @@ if __name__=='__main__':
     environment = Environment()
     
     addr_management = Address_Management(**simu_params)
-
+    code_generation_method = lambda: addr_management.generate_instruction_sequence(address_x = address_x)
     #history, this class is used by the goal generator, explorer_random and explorer_imgep
     history = History(capacity=capacity)
+
     #goal generation
     goalgenerator = GoalGenerator(history)
 
@@ -90,17 +70,18 @@ if __name__=='__main__':
     mixing_method = Mix_sequences_interleaved(max_cycle)
 
     distance_method = DistanceMethod(distance_function)
-    #Goal achievement strategy
-    policy = OptimizationPolicykNN(mutation_method,
-                                k=k,
-                                distance_method=distance_method,
-                                mixing_method = mixing_method)
 
+    
+    run_imgep(N_init=N_init,
+            N=N,
+            capacity=capacity,
+            k=k,
+            environment = environment,
+            history=history,
+            code_generation_method = code_generation_method,
+            goal_generator=goalgenerator,
+            distance_method=distance_method,
+            mutation_method=mutation_method,
+            mixing_method=mixing_method,
+            print_freq=print_freq)
 
-    #Explorer for random exploration
-    explorer_random = randomexploration(N_init,environment,lambda: addr_management.generate_instruction_sequence(address_x = address_x),history,print_freq=print_freq)
-    #IMGEP explorer
-    explorer_imgep = IMGEP(N,N_init,environment,history,goalgenerator,policy,explorer_random,print_freq=print_freq)
-
-    #Run exploration
-    explorer_imgep()
--- option1/imgep.py
+++ option1/imgep.py
@@ -73,3 +73,47 @@ class IMGEP:
     def take(self,content,count):
         self.start = count
         self.history.take(content,count)
+
+
+
+class Randomexploration:
+    def __init__(self,N,environment, generator,history:History,print_freq:int=1000):
+        self.generator = generator
+        self.environment = environment
+        self.N = N
+        self.history = history
+        self.print_freq = print_freq
+    def __call__(self):
+        for j in range(self.N):
+            if (j+1)%self.print_freq==0:
+                print(f'step {j+1}/{self.N}')
+            parameter = self.generator()
+            obs = self.environment(parameter)
+            self.history.store(parameter,obs)
+
+
+def run_imgep(N_init:int,
+        N:int,
+        capacity:int,
+        k:int,
+        environment,
+        history:History,
+        code_generation_method,
+        goal_generator,
+        distance_method,
+        mutation_method,
+        mixing_method,
+        print_freq:int):
+
+    policy = OptimizationPolicykNN(mutation_method,
+                                k=k,
+                                distance_method=distance_method,
+                                mixing_method = mixing_method)
+
+    #Explorer for random exploration
+    explorer_random = Randomexploration(N_init,environment,code_generation_method,history,print_freq=print_freq)
+    #IMGEP explorer
+    explorer_imgep = IMGEP(N,N_init,environment,history,goal_generator,policy,explorer_random,print_freq=print_freq)
+
+    #Run exploration
+    explorer_imgep()
