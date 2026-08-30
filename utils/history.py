c2ffa4de15c3be463d33d674975b1ac4fb021dc9
Ludoviccccc <ludovic.matar@yahoo.com>
2026-08-31 01:45:57 +0200

    track combinations vector values

--- utils/history.py
+++ utils/history.py
@@ -4,6 +4,18 @@ import os.path
 import copy
 import random
 import pandas as pd
+
+
+from itertools import combinations
+
+def get_all_subsets(input_list):
+    subsets = []
+    for r in range(len(input_list) + 1):
+        # combinations() returns tuples, so we convert them to lists
+        for combo in combinations(input_list, r):
+            subsets.append(list(combo))
+    return subsets
+
 class History:
     def __init__(self,
                     capacity:int=10000,
@@ -23,7 +35,17 @@ class History:
                              'L2_cache_interference']
         self.memory_components = []
         self.memory_micro_components = []
-
+        self.memory_combinations = {}
+        power_set = get_all_subsets(list(range(len(self.components))))
+        self.combinations = get_all_subsets(self.components)
+        for sub in power_set:
+            chain = ''
+            for j in range(4):
+                if j in sub:
+                    chain+='1'
+                else:
+                    chain+='0'
+            self.memory_combinations[chain] = []
 
     def __eq__(self,other):
         return self.__dict__== other.__dict__
@@ -38,11 +60,7 @@ class History:
         '''
         if self._j==0:
             raise TypeError("no element stored yet")
-        #print('type',if_type)
-        #print(self.memory_observation[if_type])
-        #print(self.memory_observation[if_type].values.shape)
         out = self.memory_observation[if_type].values
-        #print('as tab', out.shape)
         return out
     def __len__(self):
         return len(self.memory_parameter)
@@ -52,32 +70,32 @@ class History:
         #saves codes
         self.memory_parameter.append(parameter)
         #saves interference events and their type
+        combination_vector = []
+
         for if_type in obs[0]:
-            if if_type in self.memory_observation:
-                #print('shape 2',np.array(list(obs[0][if_type].values())).shape)
-                if len(list(obs[0][if_type].values())[0])>0:
-                    tab = np.array(list(obs[0][if_type].values()))
-                    tab = np.reshape(tab,(len(tab),-1))[:,0]
-                    #self.memory_observation[if_type][self._j] = tab
+            if len(list(obs[0][if_type].values())[0])>0:
+                tab = np.array(list(obs[0][if_type].values()))
+                tab = np.reshape(tab,(len(tab),-1))[:,0]
+                if if_type in self.memory_observation:
                     self.memory_observation_temp[if_type].append(tab)
                     self.memory_observation_idx_temp[if_type].append(self._j)
-            else:                             
-                if len(list(obs[0][if_type].values())[0])>0:
-                    tab = np.array(list(obs[0][if_type].values()))
-                    tab = np.reshape(tab,(len(tab),-1))[:,0]
-                    #print('elem',tab)
+                else:                             
+                    #if if_type has never been stored yet, one creates a key and a dataframe in self.memory_observation
                     self.memory_observation[if_type] = pd.DataFrame(index = obs[0][if_type].keys())
-                    ##print(np.array(list(obs[0][if_type].values())).shape)
                     self.memory_observation[if_type][self._j] = tab
                     self.memory_observation_temp[if_type] = []
                     self.memory_observation_idx_temp[if_type] = []
+
         # saves the combination of components that are involved
         encod = np.zeros((4,))
+        binary_rep = ''
         for j,component in enumerate(self.components):
-            #print(obs[0])
-            #exit()
             if len(obs[0][component][list(obs[0][component].keys())[0]])>0:
                 encod[j] = 1
+                binary_rep +='1'
+            else:
+                binary_rep +='0'
+        self.memory_combinations[binary_rep].append(combination_vector)
         self.memory_components.append(encod)
         # saves the microcomponents that are involved with one hot encoding
         self.memory_micro_components.append(obs[1])
